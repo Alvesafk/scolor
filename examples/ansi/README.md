c93f22dc167eb741999b0a650c699fdec4d0c346
Miguel de Almeida Alves <migueldealmeidaalves55@gmail.com>
2026-06-23 16:29:16 -0300

    new ansi example and README for it

---
+++ examples/ansi/README.md
@@ -0,0 +1,4 @@
+# Ansi package example
+You can check how the code is used on the `.go`.
+
+![Ansi Example](ansi.gif)
---
+++ examples/ansi/ansi_example.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Alvesafk/scolor/ansi"
+)
+
+func main() {
+	ansi.ABlue.BgPrintln("Hello, world! Using the terminal Blue!")
+	time.Sleep(1 * time.Second / 2)
+
+	ansi.ACyan.FgPrintln("Hello, world! Using the terminal Cyan!")
+	time.Sleep(1 * time.Second / 2)
+
+	ansi.AGreen.BgPrintln("Hello, world! Using the terminal Green!")
+	time.Sleep(1 * time.Second / 2)
+
+	ansi.APurple.FgPrintln("Hello, world! Using the terminal Purple!")
+	time.Sleep(1 * time.Second / 2)
+
+	ansi.ARed.BgPrintln("Hello, world! Using the terminal Red!")
+	time.Sleep(1 * time.Second / 2)
+
+	ansi.AYellow.FgPrintln("Hello, world! Using the terminal Yellow!")
+	time.Sleep(1 * time.Second / 2)
+
+	ansi.AWhite.BgPrintln("Hello, world! Using the terminal White!")
+	time.Sleep(1 * time.Second / 2)
+
+	fmt.Println(ansi.FgRainbow("Hello, world! Rainbow string with the terminal colors!", "none", 0))
+	fmt.Println(ansi.BgRainbow("Hello, world! Rainbow string with the terminal colors!", "none", 0))
+	time.Sleep(1 * time.Second / 2)
+}
